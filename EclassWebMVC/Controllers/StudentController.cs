708a1f2d946521f5bb24cae2e5bfd47849b5bfb5
haizz989898 <70678584+haizz989898@users.noreply.github.com>
2020-10-09 14:11:39 +0700

    Add project files.

---
+++ EclassWebMVC/Controllers/StudentController.cs
@@ -0,0 +1,120 @@
+﻿using EclassWebMVC.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EclassWebMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        public ActionResult Index()
+        {
+            List<Student> students = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44380/api/Students/");
+                var getTask = client.GetAsync("Get");
+                getTask.Wait();
+                var result = getTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    string data = result.Content.ReadAsStringAsync().Result;
+                    students = JsonConvert.DeserializeObject<List<Student>>(data);
+                }
+            }
+            return View(students);
+        }
+        public ActionResult Create(Student s)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44380/api/Students/");
+                string data = JsonConvert.SerializeObject(s);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                var postTask = client.PostAsync("AddStudent", content);
+                postTask.Wait();
+                var result = postTask.Result;
+                if(result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }    
+            }
+            return View();
+        }
+        public ActionResult Edit(string id)
+        {
+            Student s = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44380/api/Students/");
+                var getTask = client.GetAsync("Get/"+id);
+                getTask.Wait();
+                var result = getTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    string data = result.Content.ReadAsStringAsync().Result;
+                    s = JsonConvert.DeserializeObject<Student>(data);
+                }
+            }
+            return View("Edit",s);
+        }
+        [HttpPost]
+        public ActionResult Edit(Student s)
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44380/api/Students/");
+                string data = JsonConvert.SerializeObject(s);
+                StringContent content = new StringContent(data, Encoding.UTF8, "application/json");
+                var postTask = client.PostAsync("EditStudent/" + s.StudentID, content);
+                postTask.Wait();
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View();
+        }
+        public ActionResult Delete(string id)
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44380/api/Students/");
+                var deleteTask = client.DeleteAsync("Delete/" + id);
+                deleteTask.Wait();
+                var result = deleteTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Index");
+        }
+        public ActionResult Details(string id)
+        {
+            Student s = null;
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44380/api/Students/");
+                var getTask = client.GetAsync("Get/" + id);
+                getTask.Wait();
+                var result = getTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    string data = result.Content.ReadAsStringAsync().Result;
+                    s = JsonConvert.DeserializeObject<Student>(data);
+                }
+            }
+            return View("Details", s);
+        }
+    }
+}
